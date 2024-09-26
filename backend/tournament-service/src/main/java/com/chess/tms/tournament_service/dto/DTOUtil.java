4c93f9e0b5bc53714e3437ad537164fda2f6be2f
Xavier Tey <teystyxavy@gmail.com>
2024-09-26 14:09:51 +0800

    chore: aquiring user id from header from jwt token and passing into tournament endpoints - xavier

---
+++ backend/tournament-service/src/main/java/com/chess/tms/tournament_service/dto/DTOUtil.java
@@ -0,0 +1,48 @@
+package com.chess.tms.tournament_service.dto;
+
+import com.chess.tms.tournament_service.model.Tournament;
+import com.chess.tms.tournament_service.model.TournamentPlayer;
+
+public class DTOUtil {
+        //helper functions
+    public static TournamentDetailsDTO convertEntryToDTO(Tournament tournament) {
+        TournamentDetailsDTO dto = new TournamentDetailsDTO();
+        dto.setId(tournament.getTournamentId());
+        dto.setName(tournament.getName());
+        dto.setCreatorId(tournament.getCreatorId());
+        dto.setStartDate(tournament.getStartDate());
+        dto.setEndDate(tournament.getEndDate());
+        dto.setMinElo(tournament.getMinElo());
+        dto.setMaxElo(tournament.getMaxElo());
+        dto.setTotalPlayers(tournament.getTotalPlayers());
+        dto.setCurrentPlayers(tournament.getCurrentPlayers());
+        dto.setStatus(tournament.getStatus());
+        dto.setTimeControl(tournament.getTimeControl());
+
+        return dto;
+    }
+
+    public static PlayerRegistrationDTO convertPlayerEntrytoPlayerDTO(TournamentPlayer player) {
+        PlayerRegistrationDTO dto = new PlayerRegistrationDTO();
+        dto.setPlayerId(player.getPlayerId());
+        dto.setRegistrationStatus(player.getRegistrationStatus());
+        dto.setTournamentId(player.getTournament().getTournamentId());
+
+        return dto;
+    }
+
+    public static Tournament convertDTOToTournament(TournamentRegistrationDTO dto, long creatorId){
+        Tournament tournament = new Tournament();
+        tournament.setName(dto.getName());
+        tournament.setCreatorId(creatorId);
+        tournament.setStartDate(dto.getStartDate());
+        tournament.setEndDate(dto.getEndDate());
+        tournament.setMinElo(dto.getMinElo());
+        tournament.setMaxElo(dto.getMaxElo());
+        tournament.setTotalPlayers(dto.getTotalPlayers());
+        tournament.setCurrentPlayers(dto.getCurrentPlayers());
+        tournament.setTimeControl(dto.getTimeControl());
+
+        return tournament;
+    }
+}
