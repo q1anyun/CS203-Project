2b942656252b596c5862537bc4c592a568ad095e
q1anyun <chewqianyun2525@gmail.com>
2024-10-10 17:35:44 +0800

    feat: leaderboard
    connect to backend for overall leaderboard

--- frontend/src/components/Leaderboard/Leaderboard.jsx
+++ frontend/src/components/Leaderboard/Leaderboard.jsx
@@ -1,54 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Profile from './Profile';
 import LeaderboardHeader from './LeaderboardHeader';
-import sampleData from './data'; 
 import axios from 'axios';
 import { Container, Grid2 } from '@mui/material';
 
+const baseURL = import.meta.env.VITE_PLAYER_SERVICE_URL;
 
 function Leaderboard() {
-  // const [profiles, setProfiles] = useState([]); 
-  // const [loading, setLoading] = useState(true); 
-  // const [error, setError] = useState(null); 
+  const [profiles, setProfiles] = useState([]); 
+  const [loading, setLoading] = useState(true); 
+  const [error, setError] = useState(null); 
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get('/api/leaderboard'); 
-  //       setProfiles(response.data); 
-  //       setLoading(false); 
-  //     } catch (err) {
-  //       setError('Failed to load data'); 
-  //       setLoading(false);
-  //     }
-  //   };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${baseURL}/getTop100Players`); 
+        setProfiles(response.data); 
+        setLoading(false); 
+      } catch (err) {
+        setError('Failed to load data'); 
+        setLoading(false);
+      }
+    };
 
-  //   fetchData(); 
-  // }, []); 
+    fetchData(); 
+  }, []); 
 
-  // if (loading) return <p>Loading...</p>; 
-  // if (error) return <p>{error}</p>; 
+  if (loading) return <p>Loading...</p>; 
+  if (error) return <p>{error}</p>; 
 
   return (
     <div>
-      <LeaderboardHeader/>
-      <Container maxWidth="lg" sx={{ marginTop: 4 , marginBottom: 10}}>
+      <LeaderboardHeader />
+      <Container maxWidth="lg" sx={{ marginTop: 4, marginBottom: 10 }}>
         <Grid2 container spacing={2}>
-          {sampleData.map((profile) => (
-            <Grid2 size={12} key={profile.id}>
-              <Profile 
-                key={profile.userId}
-                rank={profile.rank}
+          {profiles.map((profile, index) => (
+            <Grid2 size={12} key={profile.userId}>
+              <Profile
+                rank={index + 1} // Set the rank as index + 1
                 firstName={profile.firstName}
                 lastName={profile.lastName}
-                username={profile.username}
-                eloPoints={profile.eloPoints}
+                eloRating={profile.eloRating}
                 profilePhoto={profile.photo}
               />
             </Grid2>
           ))}
         </Grid2>
-          {/*To create specific leaderboard position for player*/ }
+        {/* To create specific leaderboard position for player */}
       </Container>
     </div>
   );
