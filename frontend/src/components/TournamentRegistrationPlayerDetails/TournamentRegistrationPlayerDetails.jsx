779bc120343ed94ee6ca2f3640ee149080f4ec1b
q1anyun <chewqianyun2525@gmail.com>
2024-10-31 10:31:52 +0800

    refactor: tournament registration view

--- frontend/src/components/TournamentRegistrationPlayerDetails/TournamentRegistrationPlayerDetails.jsx
+++ frontend/src/components/TournamentRegistrationPlayerDetails/TournamentRegistrationPlayerDetails.jsx
@@ -1,13 +1,6 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Typography, Avatar, Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import { Typography, Avatar, Box } from '@mui/material';
 import styles from './TournamentRegistrationPlayerDetails.module.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
@@ -15,34 +8,30 @@ import { useParams, Link } from 'react-router-dom';
 
 const baseURL = import.meta.env.VITE_TOURNAMENT_PLAYER_URL;
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-        fontSize: 14,
-    },
-}));
+const DetailBox = styled(Box)({
+    backgroundColor: '#fcffff',
+    borderRadius: '8px',
+    padding: '16px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    display: 'flex',
+    alignItems: 'center',
+});
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-    },
-    '&:last-child td, &:last-child th': {
-        border: 0,
-    },
-}));
+const DetailBoxContainer = styled(Box)({
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gap: '25px',
+    marginTop: '20px',
+});
 
-function createData(id, firstName, lastName, country, eloRating, totalMatches) {
-    return { id, firstName, lastName, country, eloRating, totalMatches };
+
+function createData(id, firstName, lastName, country) {
+    return { id, firstName, lastName, country };
 }
 
-function  TournamentRegistrationPlayerDetails() {
+function TournamentRegistrationPlayerDetails() {
     const { id } = useParams();
     const [participants, setParticipants] = useState([]);
-    const [open, setOpen] = useState(false);
-    const [selectedParticipant, setSelectedParticipant] = useState(null); // Add state for the selected participant
 
     useEffect(() => {
         const fetchParticipants = async () => {
@@ -51,7 +40,7 @@ function  TournamentRegistrationPlayerDetails() {
                 const data = response.data;
                 console.log(data);
                 const formattedData = data.map((participant) =>
-                    createData(participant.id, participant.firstName, participant.lastName, participant.country, participant.eloRating, participant.totalMatches)
+                    createData(participant.id, participant.firstName, participant.lastName, participant.country)
                 );
                 setParticipants(formattedData);
             } catch (error) {
@@ -68,42 +57,28 @@ function  TournamentRegistrationPlayerDetails() {
                 <Typography variant="header1" component="h2">
                     Registered Participants
                 </Typography>
-                <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 700 }} aria-label="customized table">
-                        <TableHead>
-                            <TableRow>
-                                <StyledTableCell></StyledTableCell>
-                                <StyledTableCell>User ID</StyledTableCell>
-                                <StyledTableCell>First Name</StyledTableCell>
-                                <StyledTableCell>Last Name</StyledTableCell>
-                                <StyledTableCell>Country</StyledTableCell>
-                                <StyledTableCell>Elo Rating</StyledTableCell>
-                                <StyledTableCell>Total Matches</StyledTableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {participants.map((row) => (
-                                <StyledTableRow key={row.id} hover>
-                                    <Link
-                                        to={`/profileview/${row.id}`}
-                                        style={{ display: 'contents', textDecoration: 'none', color: 'inherit' }}
-                                    >
-                                        <StyledTableCell>{row.id}</StyledTableCell>
-                                        <StyledTableCell>
-                                            <Avatar alt="Profile" src={row.profilePhoto} sx={{ width: 56, height: 56, border: '1px solid' }} />
-                                        </StyledTableCell>
-                                        <StyledTableCell>{row.firstName}</StyledTableCell>
-                                        <StyledTableCell>{row.lastName}</StyledTableCell>
-                                        <StyledTableCell>{row.country}</StyledTableCell>
-                                        <StyledTableCell>{row.eloRating}</StyledTableCell>
-                                        <StyledTableCell>{row.totalMatches}</StyledTableCell>
-                                    </Link>
-                                </StyledTableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                <DetailBoxContainer>
+                    {participants.map((participant) => (
+                        <DetailBox key={participant.id}>
+                            <Avatar
+                                alt={`${participant.firstName} ${participant.lastName}`}
+                                src={participant.profilePhoto}
+                                sx={{ width: 56, height: 56, marginRight: '16px' }}
+                            />
+                            <Box>
+                                <Link
+                                    to={`/profileview/${participant.id}`}
+                                    style={{ textDecoration: 'none', color: 'inherit' }}
+                                >
+                                    <Typography variant="header3">{`${participant.firstName} ${participant.lastName}`}</Typography>
+                                </Link>
+                                <Typography variant="body1">{participant.country}</Typography>
+                            </Box>
+                        </DetailBox>
+                    ))}
+                </DetailBoxContainer>
             </div>
+
         </>
     );
 }
