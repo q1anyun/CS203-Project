0a6ab9d7cf6bde79c470af80d0accee612166dd9
Vicki Lim <vicki.lim.2023@smu.edu.sg>
2024-11-09 19:21:21 +0800

    feast: added profile picture in leaderboard

--- frontend/src/components/Leaderboard/LeaderboardHeader.jsx
+++ frontend/src/components/Leaderboard/LeaderboardHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './LeaderboardHeader.module.css';
-import { Typography, Stack, Box, } from '@mui/material';
+import { Typography, Stack, Box, Avatar, Divider} from '@mui/material';
 import { Link } from 'react-router-dom';
 
 
@@ -10,10 +10,10 @@ function LeaderboardHeader({ topPlayers }) {
         <div className={styles.container}>
 
             <div className={styles.content}>
-                <Typography variant="header1" textAlign={'center'} >Leaderboard</Typography>
+                <Typography variant="homePage" gutterBottom>Leaderboard</Typography>
 
             </div>
-
+           
 
             <Stack
                 direction="row"
@@ -25,18 +25,17 @@ function LeaderboardHeader({ topPlayers }) {
             >
                 {/* Second Place */}
                 <Link to={`/profileview/${topPlayers[1]?.playerId}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <Box sx={{ textAlign: 'center' }}>
-                        <Box sx={{
-                            backgroundColor: '#E5E7EB', // gray-200
-                            borderRadius: '50%',
-                            width: '5rem', // w-20
-                            height: '5rem', // h-20
-                            margin: '0 auto',
-                            marginBottom: '0.5rem',
-                            overflow: 'hidden'
-                        }}>
-
-                        </Box>
+                <Box sx={{ textAlign: 'center' }}>
+                <Avatar
+                            src={topPlayers[1]?.profilePhoto}
+                            alt={`${topPlayers[1]?.firstName}'s profile`}
+                            sx={{
+                                width: '5rem', // w-20
+                                height: '5rem', // h-20
+                                backgroundColor: '#E5E7EB', // gray-200
+                                margin: '0 auto 0.5rem',
+                            }}
+                        />
                         <Typography variant="header3" component='h2' sx={{ textAlign: 'center' }}>2</Typography>
                         <Typography variant="header3" sx={{ color: 'black' }}>
                             {topPlayers[1]?.firstName} {topPlayers[1]?.lastName}
@@ -53,16 +52,17 @@ function LeaderboardHeader({ topPlayers }) {
                         textAlign: 'center',
                         marginBottom: '-1rem'
                     }}>
-                        <Box sx={{
-                            backgroundColor: '#FCD34D', // yellow-400
-                            borderRadius: '50%',
-                            width: '6rem', // w-24
-                            height: '6rem', // h-24
-                            margin: '0 auto',
-                            marginBottom: '0.5rem',
-                            overflow: 'hidden'
-                        }}>
-                        </Box>
+                       
+                        <Avatar
+                            src={topPlayers[0]?.profilePhoto}
+                            alt={`${topPlayers[0]?.firstName}'s profile`}
+                            sx={{
+                                width: '6rem', 
+                                height: '6rem',
+                                backgroundColor: '#FCD34D', // gray-200
+                                margin: '0 auto 0.5rem',
+                            }}
+                        />
                         <Typography variant="header2" component='h3' sx={{ textAlign: 'center' }}>1</Typography>
                         <Typography variant="header2" sx={{ color: 'black' }}>
                             {topPlayers[0]?.firstName} {topPlayers[0]?.lastName}
@@ -77,17 +77,18 @@ function LeaderboardHeader({ topPlayers }) {
                 {/* Third Place */}
                 <Link to={`/profileview/${topPlayers[2]?.playerId}`} style={{ textDecoration: 'none', color: 'inherit' }}>
                     <Box sx={{ textAlign: 'center' }}>
-                        <Box sx={{
-                            backgroundColor: '#FDB068', // orange-300
-                            borderRadius: '50%',
-                            width: '5rem', // w-20
-                            height: '5rem', // h-20
-                            margin: '0 auto',
-                            marginBottom: '0.5rem',
-                            overflow: 'hidden'
-                        }}>
-
-                        </Box>
+                    
+                             <Avatar
+                            src={topPlayers[2]?.profilePhoto}
+                            alt={`${topPlayers[2]?.firstName}'s profile`}
+                            sx={{
+                                width: '5rem', 
+                                height: '5rem',
+                                backgroundColor: '#FDB068', // gray-200
+                                margin: '0 auto 0.5rem',
+                            }}
+                        />
+                            
                         <Typography variant="header3" component='h2' sx={{ textAlign: 'center' }}>3</Typography>
                         <Typography variant='header3' sx={{ color: 'black' }}>
                             {topPlayers[2]?.firstName} {topPlayers[2]?.lastName}
